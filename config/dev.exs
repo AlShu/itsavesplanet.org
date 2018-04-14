31e22140b918a107dab015f0db18488b3707da69
Alexander Shugurov <alex.shugurov@cloudcastlegroup.com>
2018-04-14 23:59:13 +0400

    Webpack 4

--- config/dev.exs
+++ config/dev.exs
@@ -11,8 +11,9 @@ config :itsavesplanet_org, ITSPWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  watchers: [
+    node: ["node_modules/webpack/bin/webpack.js", "--watch-stdin", "--progress", "--colors"]
+  ]
 
 # ## SSL Support
 #
